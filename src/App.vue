c3231275352fe7f45e0a654741e7b96be520285b
Markus Oberlehner <markus.oberlehner@gmail.com>
2022-11-29 19:52:31 +0100

    Implement solution

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,57 @@
 <template>
-  <div class="w-full h-full flex justify-center items-center">
-    <span class="text-3xl">Good luck!</span>
+  <div class="w-full h-full p-4 flex justify-center items-center">
+    <div class="max-w-xs w-full flex flex-col">
+      <div v-if="setup" data-qa="setup" class="w-3/4 p-4 rounded-2xl bg-teal-800 text-white self-start">
+        {{ setup }}
+      </div>
+      <div
+        v-if="delivery && isDeliveryVisible"
+        data-qa="delivery"
+        class="w-3/4 mt-2 p-4 rounded-2xl bg-red-800 text-white self-end"
+      >
+        {{ delivery }}
+      </div>
+      <button
+        v-if="isDeliveryVisible"
+        class="w-full bg-blue-900 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 focus:ring-offset-blue-50 text-white font-semibold h-12 px-6 rounded-lg flex items-center justify-center mx-auto mt-4"
+        @click="loadJoke"
+      >
+        Another
+      </button>
+      <button
+        v-else-if="setup"
+        class="w-full bg-blue-900 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-offset-2 focus:ring-offset-blue-50 text-white font-semibold h-12 px-6 rounded-lg flex items-center justify-center mx-auto mt-4"
+        @click="showDelivery"
+      >
+        Tell Me!
+      </button>
+    </div>
   </div>
 </template>
 
 <script setup>
-// fetch('https://v2.jokeapi.dev/joke/christmas') ...
+import { ref } from 'vue'
+
+const setup = ref(null)
+const delivery = ref(null)
+const isDeliveryVisible = ref(false)
+
+const showDelivery = () => {
+  isDeliveryVisible.value = true
+}
+
+const loadJoke = () => {
+  isDeliveryVisible.value = false
+  setup.value = null
+  delivery.value = null
+
+  fetch('https://v2.jokeapi.dev/joke/christmas')
+    .then(response => response.json())
+    .then(data => {
+      setup.value = data.setup
+      delivery.value = data.delivery
+    })
+}
+
+loadJoke()
 </script>
